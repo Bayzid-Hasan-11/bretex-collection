1f903bf900c256d4b9560102464d10629635aeb2
Bayzid-Hasan-11 <bayzid.hasan1102@gmail.com>
2026-07-27 09:46:22 +0600

    Switch database to SQLite for Render deployment

--- backend/settings.py
+++ backend/settings.py
@@ -80,12 +80,8 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ecommerce_db',
-        'USER': 'postgres',
-        'PASSWORD': 'BhEmon17', # Replace this with the password you wrote down!
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
